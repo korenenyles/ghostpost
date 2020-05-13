f97fad164f3b60c35477ffc8353b9cd6c964b7f7
Koren Nyles <korenenyles@gmail.com>
2020-05-13 15:33:37 -0400

    adds likes

--- posts/urls.py
+++ posts/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.index),
+    path('',views.index, name='home'),
     path('post_detail/<int:id>', views.post_detail, name='post_detail'),
-    path('like/<int:post_id/>', views.like_view),
+    path('like/<int:post_id>/', views.like_view),
+    path('form/', views.add_post, name='form')
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, reverse, HttpResponseRedirect
 from posts.models import PostItem
+from posts.forms import AddPost
 # Create your views here.
 def index(request):
     data = PostItem.objects.all()
@@ -10,9 +11,19 @@ def post_detail(request, id):
     return render(request, 'post_detail.html', {'post': post})
 
 
+def add_post(request):
+    html = "generic_form.html"
+    form = AddPost()
+    if request.method == "POST":
+        form = AddPost(request.POST)
+        form.save()
+        return HttpResponseRedirect(reverse("home"))
+
+    return render(request, html, {"form": form})
+
 def like_view(request, post_id):
     post = PostItem.objects.get(id=post_id)
     post.likes += 1
     post.save()
     
-    return render(request, 'index.html',)
+    return HttpResponseRedirect(reverse('post_detail', kwargs={'id': post_id}))
