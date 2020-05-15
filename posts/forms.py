db11302f6cb98cb4b365b00ba44971797122cc9c
Koren Nyles <korenenyles@gmail.com>
2020-05-15 09:13:01 -0400

    meets rubric expectations will work on extra credit

--- posts/forms.py
+++ posts/forms.py
@@ -4,5 +4,6 @@ from posts.models import PostItem
 class AddPost(forms.ModelForm):
     class Meta:
         model = PostItem
+        #Matt Perry suggested that boast_or_roast go into fields, rather than declaring it outside of meta to fix bug! 
         fields = ['post_title', 'body', 'boast_or_roast']
     
